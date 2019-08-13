5ed2ac5a48a6ac6d061f7ca0caf729e1e4ce2d88
Sara McCombs <thesaramccombs@gmail.com>
2019-08-13 20:48:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase 
+  if phrase == ""
+  elsif phrase == phrase.upcase 
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
